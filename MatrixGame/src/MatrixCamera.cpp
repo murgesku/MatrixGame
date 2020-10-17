4bd9eb0c800eeaca6539e7a6e922c75ead54b502
KIaxons <themarsvolta.televators@gmail.com>
2020-10-17 18:05:23 +0300

    Применяем новый параметр при старте ПБ

--- MatrixGame/src/MatrixCamera.cpp
+++ MatrixGame/src/MatrixCamera.cpp
@@ -659,7 +659,7 @@ CMatrixCamera::CMatrixCamera(void)
     for (int i=0; i<CAMERA_PARAM_CNT; ++i)
     {
         m_AngleParam[i] = g_Config.m_CamParams[i].m_CamAngleParam;
-		m_DistParam[i] = 1.0f;
+        m_DistParam[i] = g_Config.m_CamParams[i].m_CamDistDefault;
     }
 
     m_AngleX = LerpAng();
