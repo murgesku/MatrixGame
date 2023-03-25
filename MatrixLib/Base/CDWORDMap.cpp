6f833361246e9901325579ba699f730deaeb6cc5
Vlad Reutski <reutski.v@gmail.com>
2023-03-25 21:50:25 +0200

    update CDWORDMap

--- MatrixLib/Base/CDWORDMap.cpp
+++ MatrixLib/Base/CDWORDMap.cpp
@@ -3,16 +3,14 @@
 // Licensed under GPLv2 or any later version
 // Refer to the LICENSE file included
 
-#include "Base.pch"
-
 #include "CDWORDMap.hpp"
 
 namespace Base {
 
-void CDWORDMap::Enum(ENUM_DWORD f, DWORD user) {
+void CDWORDMap::Enum(ENUM_DWORD f, uint32_t user) {
     for (int i = 0; i < HASH_TABLE_SIZE; ++i) {
         if (m_Table[i]) {
-            for (DWORD j = 0; j < m_Table[i]->contained; ++j) {
+            for (uint32_t j = 0; j < m_Table[i]->contained; ++j) {
                 if (!f((m_Table[i] + 1 + j)->key, (m_Table[i] + 1 + j)->value, user))
                     return;
             }
