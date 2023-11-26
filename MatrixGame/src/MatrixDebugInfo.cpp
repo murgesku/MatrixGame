4d3128d61d5fd3e028acb92db03e83c910c796d1
exweilt <exwelt@posteo.net>
2023-11-26 11:46:18 +0000

    add background to console, increase font

--- MatrixGame/src/MatrixDebugInfo.cpp
+++ MatrixGame/src/MatrixDebugInfo.cpp
@@ -4,14 +4,20 @@
 // Refer to the LICENSE file included
 
 #include "MatrixDebugInfo.hpp"
+#include "3g.hpp"
 #include <stdexcept>
 #include <cstdint>
 
+#include <stupid_logger.hpp>
+
 #define DI_KEY_X 10
 #define DI_KEY_Y 10
-#define DI_KEY_W 140
-#define DI_KEY_H 14
-#define DI_VAL_W 200
+#define DI_KEY_W 240
+#define DI_KEY_H 19
+#define DI_VAL_W 400
+#define DI_BG_HORIZONTAL_OFFSET 40
+#define DI_AVERAGE_CHAR_WIDTH 9     // Can be found with m_Font->GetTextMetricsA(&tm);
+#define DI_BG_COLOR 0x40000000
 
 #define MAX_DEBUG_INFO_ITEMS 128
 
@@ -21,8 +27,8 @@ CMatrixDebugInfo::CMatrixDebugInfo()
 : m_Font{nullptr}
 , m_Pos{DI_KEY_X, DI_KEY_Y}
 {
-    if (D3D_OK != D3DXCreateFontW(g_D3DD, 10, 0, FW_NORMAL, 1, FALSE, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS,
-                                  DEFAULT_QUALITY, DEFAULT_PITCH, L"MS Sans Serif", &m_Font))
+    if (D3D_OK != D3DXCreateFontW(g_D3DD, 25, 0, FW_NORMAL, 1, FALSE, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS,
+                                  PROOF_QUALITY, DEFAULT_PITCH, L"MS Sans Serif", &m_Font))
     {
         throw std::runtime_error("Failed to create font with D3DXCreateFontW()");
     }
@@ -37,6 +43,8 @@ void CMatrixDebugInfo::Draw()
 {
     try
     {
+        int max_value_len = 0;
+
         int y = m_Pos.y;
         for (auto& item : m_Items)
         {
@@ -67,8 +75,44 @@ void CMatrixDebugInfo::Draw()
             m_Font->DrawTextW(NULL, item.val.c_str(), item.val.length(), (LPRECT)&r,
                               DT_LEFT | DT_VCENTER | DT_SINGLELINE | DT_NOCLIP, color);
 
+            if (item.val.length() > max_value_len)
+                max_value_len = item.val.length();
+
             y += DI_KEY_H;
         }
+
+        if (m_Items.size() > 0) {
+            // Draw rect-pad background
+
+            D3DXVECTOR2 left_top{0, 0};
+            D3DXVECTOR2 right_bot{
+                    static_cast<FLOAT>(DI_KEY_W + DI_BG_HORIZONTAL_OFFSET + max_value_len * DI_AVERAGE_CHAR_WIDTH),
+                    static_cast<FLOAT>((DI_KEY_Y + DI_KEY_H * 2) + DI_KEY_H * m_Items.size())
+            };
+
+            D3DXVECTOR4 tri1[3];
+            tri1[0] = D3DXVECTOR4(left_top.x, left_top.y, 0.5f, 1.0f);
+            tri1[1] = D3DXVECTOR4(right_bot.x, left_top.y, 0.5f, 1.0f);
+            tri1[2] = D3DXVECTOR4(right_bot.x, right_bot.y, 0.5f, 1.0f);
+
+            D3DXVECTOR4 tri2[3];
+            tri2[0] = D3DXVECTOR4(left_top.x, left_top.y, 0.5f, 1.0f);
+            tri2[1] = D3DXVECTOR4(right_bot.x, right_bot.y, 0.5f, 1.0f);
+            tri2[2] = D3DXVECTOR4(left_top.x, right_bot.y, 0.5f, 1.0f);
+
+            g_D3DD->SetRenderState(D3DRS_TEXTUREFACTOR, DI_BG_COLOR);
+
+            SetAlphaOpSelect(0, D3DTA_TFACTOR);
+            SetColorOpSelect(0, D3DTA_TFACTOR);
+
+            g_D3DD->SetRenderState(D3DRS_ALPHABLENDENABLE, TRUE);
+
+            g_D3DD->SetFVF(D3DFVF_XYZRHW);
+
+            g_D3DD->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 1, &tri1, sizeof(D3DXVECTOR4));
+            g_D3DD->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 1, &tri2, sizeof(D3DXVECTOR4));
+        }
+
     }
     catch (...)
     {
